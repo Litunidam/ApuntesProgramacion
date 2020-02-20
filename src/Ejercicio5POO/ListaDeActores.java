21113004ad5e32e67d0a98101dc4f64ef2f7b53e
lituni <manuelmarb@gmail,com>
2020-02-20 20:10:53 +0100

    Format

--- src/Ejercicio5POO/ListaDeActores.java
+++ src/Ejercicio5POO/ListaDeActores.java
@@ -2,57 +2,55 @@ package Ejercicio5POO;
 
 import java.util.ArrayList;
 
-
 public class ListaDeActores {
-		
-		private ArrayList<String>l=new ArrayList<>();
-		
-		public void setNombre(String s) {
-			if (l.contains(s)) {
-				System.out.println("El nombre: "+s+" ya está en la lista");
-			}else {
-				l.add(s);
-			}
-		}
-		
-		public void changeNombre(String s,String c) {
-			if(l.contains(s)) {
-				l.set(l.indexOf(s),c);
-			}else {
-				System.out.println("Ese nombre no existe");
-			}
-		}
-		
-		public void deleteNombre(String s) {
-			if(l.contains(s)) {
-				l.remove(s);
-			}else {
-				System.out.println("Ese nombre no está en la base de datos");
-			}
-		}
-		
-		public int searchNombre(String s) {
-			if(l.contains(s)) {
-				
-			return l.indexOf(s);
-				
-			}else {
-				System.out.println("No existe ese nombre en la base de datos");
-				return -1;
-			}
-						
-		}
-		public void showNombres() {
-			for(String s:l) {
-				System.out.println(s);
-			}
-		}
-		
-		public int countNombres() {
-			
-			return l.size();
-			}
 
+	private ArrayList<String> l = new ArrayList<>();
+
+	public void setNombre(String s) {
+		if (l.contains(s)) {
+			System.out.println("El nombre: " + s + " ya está en la lista");
+		} else {
+			l.add(s);
+		}
+	}
+
+	public void changeNombre(String s, String c) {
+		if (l.contains(s)) {
+			l.set(l.indexOf(s), c);
+		} else {
+			System.out.println("Ese nombre no existe");
+		}
+	}
+
+	public void deleteNombre(String s) {
+		if (l.contains(s)) {
+			l.remove(s);
+		} else {
+			System.out.println("Ese nombre no está en la base de datos");
+		}
+	}
+
+	public int searchNombre(String s) {
+		if (l.contains(s)) {
+
+			return l.indexOf(s);
+
+		} else {
+			System.out.println("No existe ese nombre en la base de datos");
+			return -1;
+		}
+
+	}
+
+	public void showNombres() {
+		for (String s : l) {
+			System.out.println(s);
+		}
+	}
+
+	public int countNombres() {
+
+		return l.size();
+	}
 
 }
-
--- src/Ejercicio5POO/Principal.java
+++ src/Ejercicio5POO/Principal.java
@@ -4,74 +4,74 @@ import java.util.Scanner;
 
 public class Principal {
 	private static Scanner s = new Scanner(System.in);
+
 	public static void menu(ListaDeActores l) {
-		
+
 		int option;
-		do{
-		System.out.println();
-		System.out.println("1) Para agregar Nombres");
-		System.out.println("2) Para modificar Nombres");
-		System.out.println("3) Para borrar Nombres");
-		System.out.println("4) Para buscar Nombre y en que posición esta");
-		System.out.println("5) Para visualizar Nombres");
-		System.out.println("6) Cantidad de actores");
-		System.out.println("7) Salir");
-		System.out.println("-----------------------------------------------");
-		System.out.println("Seleccione opción:");
-		
-		option=s.nextInt();
-		
-		}while(option<1 && option>7);
+		do {
+			System.out.println();
+			System.out.println("1) Para agregar Nombres");
+			System.out.println("2) Para modificar Nombres");
+			System.out.println("3) Para borrar Nombres");
+			System.out.println("4) Para buscar Nombre y en que posición esta");
+			System.out.println("5) Para visualizar Nombres");
+			System.out.println("6) Cantidad de actores");
+			System.out.println("7) Salir");
+			System.out.println("-----------------------------------------------");
+			System.out.println("Seleccione opción:");
+
+			option = s.nextInt();
+
+		} while (option < 1 && option > 7);
 		s.nextLine();
-		
-		comenzar(option,l);
+
+		comenzar(option, l);
 	}
-	
-	public static void comenzar(int option,ListaDeActores l) {
-	
+
+	public static void comenzar(int option, ListaDeActores l) {
+
 		switch (option) {
-			case 1:
-				System.out.println("Introduzca el nombre a insertar:");
-				String c=s.nextLine();
-				l.setNombre(c);
-				break;
-			case 2:
-				System.out.println("Introduzca el nombre que quieres modificar:");
-				String p=s.nextLine();
-				System.out.println("Ahora introduzca el nuevo nombre");
-				String np=s.nextLine();
-				l.changeNombre(p, np);
-				break;
-			case 3:
-				System.out.println("Inserte el nombre que quieras borrar");
-				String n=s.nextLine();
-				l.deleteNombre(n);
-				break;
-			case 4:
-				System.out.println("Inserte el nombre para saber su posición");
-				String buscar=s.nextLine();
-				int position=l.searchNombre(buscar);
-				break;
-			case 5:
-				l.showNombres();
-				break;
-			case 6:
-				int cantidad = l.countNombres();
-				break;
-			case 7:
-				break;
-		
+		case 1:
+			System.out.println("Introduzca el nombre a insertar:");
+			String c = s.nextLine();
+			l.setNombre(c);
+			break;
+		case 2:
+			System.out.println("Introduzca el nombre que quieres modificar:");
+			String p = s.nextLine();
+			System.out.println("Ahora introduzca el nuevo nombre");
+			String np = s.nextLine();
+			l.changeNombre(p, np);
+			break;
+		case 3:
+			System.out.println("Inserte el nombre que quieras borrar");
+			String n = s.nextLine();
+			l.deleteNombre(n);
+			break;
+		case 4:
+			System.out.println("Inserte el nombre para saber su posición");
+			String buscar = s.nextLine();
+			int position = l.searchNombre(buscar);
+			break;
+		case 5:
+			l.showNombres();
+			break;
+		case 6:
+			int cantidad = l.countNombres();
+			break;
+		default:
+			break;
+
 		}
-		
+
 	}
+
 	public static void main(String[] args) {
-		
+
 		ListaDeActores l = new ListaDeActores();
-		
+
 		menu(l);
-		
-		
-		
+
 	}
-	
+
 }
